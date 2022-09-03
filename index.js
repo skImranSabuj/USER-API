d248b7fed920badd38d880c03ca240bdd6518e3b
Sk Imran Sabuj <sabujx9@gmail.com>
2022-09-03 21:20:41 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+app.listen(port, () =>console.log("USER-API Server running at PORT:",port))
